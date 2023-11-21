520bbec7e6cc16e3c69eefba099a38bff6838dab
Mouawaz <99729264+Mouawaz@users.noreply.github.com>
2023-11-21 13:27:20 +0100

    SDJ Plumbing pt1

---
+++ SDJ/HousePlumbing/src/RoomWithPlumbing.java
@@ -0,0 +1,39 @@
+import java.util.Objects;
+
+public class RoomWithPlumbing extends Room {
+    private boolean floorDrain;
+
+    public RoomWithPlumbing(String type, double size,boolean hasFoorDrain){
+        super(type,size);
+        this.floorDrain=hasFoorDrain;
+    }
+    public boolean hasFloorDrain(){
+        return floorDrain;
+    }
+    public void addFloorDrain(){
+        this.floorDrain=true;
+    }
+
+    @Override
+    public String toString() {
+        return "RoomWithPlumbing{" +
+                "floorDrain=" + floorDrain +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        RoomWithPlumbing that = (RoomWithPlumbing) o;
+        return floorDrain == that.floorDrain;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(floorDrain);
+    }
+
+    //to add copy
+}
