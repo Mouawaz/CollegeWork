5baf4cb64ebfed9961ec3fb5fb51b0de737b44f5
Luca <99729264+Mouawaz@users.noreply.github.com>
2023-11-24 21:26:31 +0200

    SiteCollectioon push

--- SDJ/SiteCollection/src/SiteCollection.java
+++ SDJ/SiteCollection/src/SiteCollection.java
@@ -11,5 +11,28 @@ public class SiteCollection {
     public int getNumberOfSites() {
         return size;
     }
+
+    public void addSite(WebSite site){
+        webSites[size] = site;
+        size++;
+    }
+
+    public void removeSite(String url){
+        int index = -1;
+        for (int i = 0; i < size; i++)
+        {
+            if(webSites[i].getUrl().equals(url)){
+                index = i;
+            }
+        }
+        if(index != -1){
+            for (int i = index; i < size - 1; i++) {
+                webSites[i] = webSites[i + 1];
+            }
+            webSites[size - 1] = null;
+            size--;
+        }
+    }
+    
          
 }
