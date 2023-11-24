75ae71184952e7c5fc7edf1a14f2e9b58de43a58
Mouawaz <99729264+Mouawaz@users.noreply.github.com>
2023-11-24 08:55:21 +0100

    website push2

---
+++ SDJ/SiteCollection/src/SiteCollection.java
@@ -0,0 +1,15 @@
+public class SiteCollection {
+    private int size;
+    private static final int MaxSites=25;
+    WebSite[] webSites;
+
+    public SiteCollection(){
+        this.size = 0;
+        webSites = new WebSite[25];
+    }
+
+    public int getNumberOfSites() {
+        return size;
+    }
+         
+}
--- SDJ/SiteCollection/src/WebSite.java
+++ SDJ/SiteCollection/src/WebSite.java
@@ -1,2 +1,35 @@
-public class WebSite {
+import java.util.Objects;
+
+public class WebSite{
+    private String url;
+    private boolean userAccess;
+
+    public WebSite(String url,boolean access){
+        this.url=url;
+        this.userAccess=access;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setAccess(boolean userAccess) {
+        this.userAccess = userAccess;
+    }
+    public boolean hasAccess(){
+        return userAccess;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WebSite webSite = (WebSite) o;
+        return userAccess == webSite.userAccess && Objects.equals(url, webSite.url);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(url, userAccess);
+    }
 }
